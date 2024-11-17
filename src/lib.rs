847475e84e3958a96f68a953e796b1b46cf594fa
Mateusz Polito <matipolit@gmail.com>
2024-11-17 14:14:21 +0100

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,120 @@
+use std::{fs, process::Command};
+
+use abi_stable::std_types::{ROption, RString, RVec};
+use anyrun_plugin::*;
+use serde::Deserialize;
+use weather::{Coord, WeatherResponse};
+
+mod weather;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    prefix: String,
+    weather_location: weather::Coord,
+    openweatherapi_key: String,
+    units: weather::WeatherUnits,
+}
+
+pub struct State {
+    config: Option<Config>,
+    city_id: Option<i32>,
+}
+
+#[init]
+fn init(config_dir: RString) -> State {
+    State {
+        config: match fs::read_to_string(format!("{}/weather.ron", config_dir)) {
+            Ok(content) => Some(ron::from_str(&content).unwrap()),
+            Err(why) => {
+                eprintln!("Error reading Weather config file: {}", why);
+                None
+            }
+        },
+        city_id: None,
+    }
+}
+
+#[info]
+fn info() -> PluginInfo {
+    PluginInfo {
+        name: "Weather".into(),
+        icon: "weather".into(), // Icon from the icon theme
+    }
+}
+
+#[get_matches]
+fn get_matches(input: RString, state: &mut State) -> RVec<Match> {
+    let input = if let Some(input) = input.strip_prefix(":wttr") {
+        input.trim()
+    } else {
+        return RVec::new();
+    };
+
+    if let Some(config) = &state.config {
+        //println!("Running request");
+        let response = reqwest::blocking::get(format!(
+            "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}&units={}",
+            config.weather_location.lat,
+            config.weather_location.lon,
+            config.openweatherapi_key,
+            config.units.to_string()
+        ));
+        //println!("Got some kind of response");
+
+        if let Ok(response) = response {
+            //println!("{:?}", response);
+            let data: WeatherResponse = response.json().unwrap();
+            state.city_id = Some(data.id);
+            vec![Match {
+                title: format!("{} {}", data.main.temp, config.units.unitSuffix()).into(),
+                icon: ROption::RSome("weather".into()),
+                use_pango: false,
+                description: ROption::RSome(RString::from(format!(
+                    "{}\nFeels like: {} {}\nHumidity: {} %\nPressure: {} hPa\n\nData for: {}",
+                    data.weather.head.description,
+                    data.main.feels_like,
+                    config.units.unitSuffix(),
+                    data.main.humidity,
+                    data.main.pressure,
+                    data.name
+                ))),
+                id: ROption::RNone, // The ID can be used for identifying the match later, is not required
+            }]
+            .into()
+        } else {
+            vec![Match {
+                title: "Error getting weather".into(),
+                icon: ROption::RSome("dialog-error".into()),
+                use_pango: false,
+                description: ROption::RSome("Error getting response from OpenWeatherAPI".into()),
+                id: ROption::RNone, // The ID can be used for identifying the match later, is not required
+            }]
+            .into()
+        }
+    } else {
+        vec![Match {
+            title: "Weather config not loaded".into(),
+            icon: ROption::RSome("dialog-error".into()),
+            use_pango: false,
+            description: ROption::RSome("Config either malformed or not created".into()),
+            id: ROption::RNone, // The ID can be used for identifying the match later, is not required
+        }]
+        .into()
+    }
+}
+
+#[handler]
+fn handler(selection: Match, state: &State) -> HandleResult {
+    if let Err(why) = Command::new("sh")
+        .arg("-c")
+        .arg(format!(
+            "xdg-open https://openweathermap.org/city/{}",
+            state.city_id.unwrap()
+        ))
+        .spawn()
+    {
+        println!("Failed to open OpenWeatherMap: {}", why);
+    }
+
+    HandleResult::Close
+}
---
+++ src/weather.rs
@@ -0,0 +1,93 @@
+use nonempty::NonEmpty;
+use serde::Deserialize;
+
+#[derive(Default, Debug, Deserialize)]
+pub enum WeatherUnits {
+    Standard,
+    #[default]
+    Metric,
+    Imperial,
+}
+
+impl std::string::ToString for WeatherUnits {
+    fn to_string(&self) -> String {
+        match self {
+            WeatherUnits::Standard => "standard".into(),
+            WeatherUnits::Metric => "metric".into(),
+            WeatherUnits::Imperial => "imperial".into(),
+        }
+    }
+}
+
+impl WeatherUnits {
+    pub fn unitSuffix(&self) -> String {
+        match self {
+            WeatherUnits::Standard => "K".into(),
+            WeatherUnits::Metric => "°C".into(),
+            WeatherUnits::Imperial => "°F".into(),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct WeatherResponse {
+    pub coord: Coord,
+    pub weather: NonEmpty<Weather>,
+    pub base: String,
+    pub main: Main,
+    pub visibility: Option<u32>,
+    pub wind: Wind,
+    pub clouds: Clouds,
+    pub dt: i64,
+    pub sys: Sys,
+    pub timezone: i32,
+    pub id: i32,
+    pub name: String,
+    pub cod: i32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Coord {
+    pub lon: f32,
+    pub lat: f32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Weather {
+    pub id: i32,
+    pub main: String,
+    pub description: String,
+    pub icon: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Main {
+    pub temp: f64,
+    pub feels_like: f64,
+    pub temp_min: f64,
+    pub temp_max: f64,
+    pub pressure: i32,
+    pub humidity: i32,
+    pub sea_level: Option<i32>,
+    pub grnd_level: Option<i32>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Wind {
+    pub speed: f64,
+    pub deg: i32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Clouds {
+    pub all: i32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Sys {
+    pub r#type: Option<i32>, // `type` is a reserved keyword, use `r#type`
+    pub id: Option<i32>,
+    pub country: String,
+    pub sunrise: i64,
+    pub sunset: i64,
+}
